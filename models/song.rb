7fd71f5db6846a0c9f1d8aa048cfd6df9cd48d02
Tripp Roberts <tripp@tripp-roberts.com>
2015-01-11 01:37:28 -0500

    Removes stream_url attribute from Song. Also removes test output from Song#recent

--- models/song.rb
+++ models/song.rb
@@ -4,24 +4,17 @@ require 'json'
 
 class Song
 
-  attr_accessor :id, :url, :title, :stream_url, :created_at
+  attr_accessor :id, :url, :title, :created_at
 
   def initialize(args={})
     @id = args[:id]
     @url = args[:url]
     @title = args[:title]
-    @stream_url = args[:stream_url]
     @created_at = args[:created_at]
   end
 
   def self.recent(limit=5)
-    SongService.recent_photos(limit).each do |song|
-      puts song.url
-      puts song.id
-      puts song.title
-      puts song.stream_url
-      puts song.created_at
-    end
+    SongService.recent_photos(limit)
   end
 
 end
